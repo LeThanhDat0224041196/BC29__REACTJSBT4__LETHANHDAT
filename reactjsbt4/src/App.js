0a1eb05aefbe71ee05b39bccc8d27844a2b536d8
LeThanhDat0224041196 <lethanhdat0224@gmail.com>
2022-07-12 16:29:03 +0700

    Uploads

---
+++ reactjsbt4/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BTDatVeXemPhim from './Components/BTDatVeXemPhim';
+
+function App() {
+  return (
+    <div className="App">
+      <BTDatVeXemPhim />
+    </div>
+  );
+}
+
+export default App;
