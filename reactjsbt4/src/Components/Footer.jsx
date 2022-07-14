343830b95a46e5da5f2067ad5b9fa6044dcdd74a
LeThanhDat0224041196 <lethanhdat0224@gmail.com>
2022-07-14 16:29:55 +0700

    First Uploads

---
+++ reactjsbt4/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+    <div className="copy-wthree">
+        <p>© 2018 Movie Seat Selection . All Rights Reserved | Design by
+          <a href="http://w3layouts.com/" target="_blank">W3layouts</a>
+        </p>
+    </div>
+    )
+  }
+}
---
+++ reactjsbt4/src/Store/reducer/TicketReducers.js
@@ -0,0 +1,8 @@
+const STATE_DEFAULT ={};
+
+export const ticketReducer = (state = STATE_DEFAULT, {type, payload})=>{
+    switch (type) {
+        default:
+            return state
+    }
+}
